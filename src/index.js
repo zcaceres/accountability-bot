3dcaf583c5e87e2364be18569f6f62deb3a606e8
zc <zachcaceres@gmail.com>
2018-01-03 20:38:01 +0800

    fix: remove old notes. fix: add proper keys

--- src/index.js
+++ src/index.js
@@ -3,29 +3,16 @@ const github = require('./github')
 
 /* global process setInterval */
 
-// Twitter API (access to Sara and Zach's twitter via api key)
-// GitHub api
-// Git repo for progress logs (submitting markdown documents each 24 period)
-// Chron job: checking every 24 hours
-
 /*
   Basic Functionality
 
   1. When timer hits midnight
   2. Hit Github API to fetch commit log of progress log directory
-  3. Get two most recent commits
-  4. Parse for date of commits
-  5. Parse USERNAMES of commits
-  6. if date === yesterday, and usernames are Sara and Zach's
-    Do nothing!
-  7. if Sara or Zach's username is not included or date is before yesterday
+  3. Get last 24 hours of commits
+  4. if Sara or Zach's username is not included or date is before yesterday
     Post message to Twitter API based on the username missing
 
   Logs Repo: https://github.com/zcaceres/progress-logs.git
-  Sara Twitter: @sarasanchezgt
-  Zach Twitter: @zachcaceres
-  Sara Github: @sisanchez
-  Zach Github: @zcaceres
 */
 
 function timer(duration) {
--- src/twitter.js
+++ src/twitter.js
@@ -2,14 +2,14 @@ const Twitter = require('twitter')
 
 function setup() {
   const zach = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    consumer_key: process.env.TWITTER_ZACH_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_ZACH_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ZACH_KEY,
     access_token_secret: process.env.TWITTER_ZACH_SECRET
   });
   const sara = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    consumer_key: process.env.TWITTER_SARA_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_SARA_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_SARA_KEY,
     access_token_secret: process.env.TWITTER_SARA_SECRET
   });
